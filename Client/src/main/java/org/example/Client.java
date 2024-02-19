bd9879040c85b72679f6a909a3d5e99959244b7a
viefG <lucianofurore99@gmail.com>
2024-02-19 17:23:41 +0100

    secondo commit

---
+++ Client/src/main/java/org/example/Client.java
@@ -0,0 +1,274 @@
+package org.example;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+
+
+    Socket mioSocket = null;
+
+    int porta = 6789;  //Porta del server
+
+    PrintWriter out;
+    BufferedReader in;
+
+    Scanner scanner = new Scanner(System.in);
+
+    BufferedReader tastiera;
+
+    public void comunicazioneIniziale() { // Questo metodo si occupa di gestire l'accesso del client, se tramite login (utente registrato o tramite anon)
+            System.out.print("Come vuoi accedere? Anon, Login ");
+            String richiesta = scanner.nextLine();
+            String lowerRichiesta=richiesta.toLowerCase(); // Ho utilizzato toLowerCase per evitare problemi quando l'utente scrive lettere maiuscole in modo da gestire anche i caratteri in maiuscolo
+            if (!lowerRichiesta.equals("login") && !lowerRichiesta.equals("anon")){
+                System.out.println("Comando sconosciuto");
+                comunicazioneIniziale();
+            }
+            System.out.println("Invio: " + richiesta);
+
+            switch (lowerRichiesta){
+                case "login" :
+                    gestisciLogin(richiesta); // Chiama il metodo che gestisce il login come utente registrato (in questo caso come in tutti gli altri si passa anche la richiesta al metodo che poi verra' passata anche al client
+                    break;
+                case "anon" :
+                    gestisciAnon(richiesta);// Chiama il metodo che gestisce il login come anonimo
+                    break;
+            }
+    }
+
+    public void comunica() { // Questo metodo e' il cuore del client, gestisce tutte le richieste che vanno mandate al server, ogni volta che si vuole fare una nuova richiesta si passa da questo metodo
+        try {
+            String richiesta = in.readLine();
+            System.out.println(richiesta);
+            String risposta=scanner.nextLine();
+            String lowerRichiesta=risposta.toLowerCase();
+            if (!lowerRichiesta.equals("list") && !lowerRichiesta.equals("download") && !lowerRichiesta.equals("rename") && !lowerRichiesta.equals("delete") && !lowerRichiesta.equals("logout") && !lowerRichiesta.equals("upload"))  { // Questo if server per assicurasi di gestire solo le richieste previste, nel caso in cui la richiesta non si tra quelle gestite in questo ciclo if, il client restitusce che il comando e' sconosciuto e chiede un nuovo comando
+                System.out.println("Comando sconosciuto");
+                out.println("errore");
+                comunica(); // Se il comando inserito non e' presente nei parametri dell'if, il client richiama il metodo comunica per riporvare
+            }
+            switch (lowerRichiesta){ // Questo switch case serve per lanciare i metodi che gestiranno le richieste del client da mandare al server
+                case "list" :
+                    stampaLista(lowerRichiesta);
+                    break;
+                case "download" :
+                    scarica(lowerRichiesta);
+                    break;
+                case "upload" :
+                    upload(lowerRichiesta);
+                    break;
+                case "rename" :
+                    rinomina(lowerRichiesta);
+                    break;
+                case "delete":
+                    cancella(lowerRichiesta);
+                    break;
+                case "logout":
+                    disconnetti(lowerRichiesta);
+                    break;
+            }
+        } catch (IOException e){
+            System.out.println("Errore "+e.getMessage());
+        }
+
+    }
+
+
+    public Socket connetti(){ // Questo metodo serve per far connettere il client al server
+        try {
+            System.out.println("provo a connettermi");
+            this.mioSocket = new Socket(InetAddress.getLocalHost(), porta);
+            System.out.println("Sono connesso");
+            in = new BufferedReader(new InputStreamReader(mioSocket.getInputStream()));
+            out = new PrintWriter(mioSocket.getOutputStream(), true);
+            comunicazioneIniziale();
+        } catch (IOException e){
+            System.out.println("errore");
+        }
+        return mioSocket;
+    }
+
+    public void gestisciLogin(String richiesta){ // Questo metodo server per mandare la richiesta di login come utente registrato al server, inviando anche il nome e la password
+        try {
+            out.println(richiesta);
+            System.out.println("Inserisci nome");
+            String nome= scanner.nextLine();
+            System.out.println("Inserisci password");
+            String pass= scanner.nextLine();
+            out.println(nome);
+            out.println(pass);
+
+
+            String risultato=in.readLine();
+
+
+            if (risultato.equals("password errata")){
+                System.out.println(risultato);
+                comunicazioneIniziale();
+            } else if (risultato.equals("Utente non trovato")){
+                System.out.println(risultato);
+                comunicazioneIniziale();
+            }
+            System.out.println(risultato);
+
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        comunica();
+    }
+
+    public void gestisciAnon(String richiesta){ // Questo metodo serve per richiede l'accesso come utente anonimo al server
+        out.println(richiesta);
+        try {
+            String risposta=in.readLine();
+            System.out.println(risposta);
+        } catch (IOException e){
+            System.out.println("errore "+e.getMessage());
+        }
+        comunica();
+    }
+
+    public void stampaLista(String richiesta){ // Questo metodo server per richiede al server la lista di comandi utulizzabile dall'uteente dal quale si e' effettuato l'accesso
+        out.println(richiesta); // Invia la richiesta al server
+
+        try{
+            while (true) {
+                String risultato = in.readLine();
+                if (risultato.equals("END")) {
+                    System.out.println("Cartella non presente, contattare assistenza");
+                    break;
+                }
+                if (!risultato.equals("finito")){
+                    System.out.println(risultato);
+                } else if (risultato.equals("finito")){
+                    comunica();
+                }
+
+            }
+        } catch (IOException e){
+            System.out.println("errore"+e.getMessage());
+        }
+        System.out.println("sono qua");
+
+    }
+
+    public void scarica(String richiesta) { // Questo metodo permette di scaricare i file dal server al disco del client, per farlo e' stata creata una nuova socket per lo scambio di dati
+        String posizione= "./Download/";
+        try {
+            out.println(richiesta);
+            System.out.println(in.readLine());
+            String nomeFile = scanner.nextLine();
+            posizione=posizione+nomeFile;
+            out.println(nomeFile);
+
+
+            int portaDati = Integer.parseInt(in.readLine()); // Aspetta la risposta del server che include la porta per la connessione dati
+
+            // Stabilisce una nuova connessione Socket per permettere il trasferimento dei dati tra il client ed il server
+            try (Socket socketDati = new Socket(mioSocket.getInetAddress(), portaDati);
+                 InputStream is = socketDati.getInputStream();
+                 FileOutputStream fos = new FileOutputStream(posizione);
+                 BufferedOutputStream bos = new BufferedOutputStream(fos)) {
+
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = is.read(buffer)) > 0) {
+                    bos.write(buffer, 0, bytesRead);
+                }
+                System.out.println("File " + nomeFile + " scaricato con successo.");
+            } catch (IOException e) {
+                System.out.println("Errore durante il download del file: " + e.getMessage());
+            }
+
+            comunica(); // Torna alla comunicazione normale dopo il download
+        } catch (IOException e) {
+            System.out.println("Errore durante la comunicazione con il server: " + e.getMessage());
+        }
+        comunica();
+    }
+
+    public void upload(String richiesta){ // Questo metodo permette al client nel caso in cui abbia effettuato l'accesso come utente di caricare file sul server
+        try {
+
+            out.println(richiesta);
+            String posizione="./DiscoDelClient/";
+            System.out.println(in.readLine());
+            String nome=scanner.nextLine();
+            posizione=posizione+nome;
+            out.println(nome);
+
+
+            int portaDati = Integer.parseInt(in.readLine());
+
+            // Connetti alla porta dati e invia il file
+            try (Socket socketDati = new Socket(mioSocket.getInetAddress(), portaDati);
+                 BufferedOutputStream bos = new BufferedOutputStream(socketDati.getOutputStream());
+                 FileInputStream fis = new FileInputStream(posizione)) {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = fis.read(buffer)) != -1) {
+                    bos.write(buffer, 0, bytesRead);
+                }
+                bos.flush();
+            }
+            System.out.println("File caricato con successo.");
+        } catch (IOException e) {
+            System.out.println("Errore durante l'upload del file: " + e.getMessage());
+        }
+        comunica();
+    }
+
+    public void rinomina(String richiesta){ // Questo metodo serve per rinominare un file presente sul server (solo gli utenti registrati posso utilizzare questo metodo)
+        try {
+            out.println(richiesta);
+            System.out.println(in.readLine());
+            String nomeFile = scanner.nextLine();
+            out.println(nomeFile);
+            System.out.println(in.readLine());
+            String nuovoNome=scanner.nextLine();
+            out.println(nuovoNome);
+            String risultato=in.readLine();
+            System.out.println(risultato);
+
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+        comunica();
+    }
+
+    public void cancella(String richiesta){ // QUesto metodo permette di eliminare un file presente sul server (Solo gli utenti registrati posso usare questo metodo)
+        try {
+            out.println(richiesta);
+            System.out.println(in.readLine());
+            String nome=scanner.nextLine();
+            out.println(nome);
+            System.out.println(in.readLine());
+            comunica();
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void disconnetti (String richiesta){ // Questo metodo permette la disconnessione dell'utente, richiamando il metodo comunicazioneIniziale per dare la possibilita' di effettuare l'accesso come anonimo o come un altro utente
+        out.println(richiesta);
+        try {
+            System.out.println(in.readLine());
+            comunicazioneIniziale();
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+
+    public static void main(String[] args) { // Il metodo main lancia il metodo connetti per avviare la socket
+        Client c = new Client();
+        c.connetti();
+    }
+}
---
+++ Server/src/main/java/org/example/Server.java
@@ -0,0 +1,325 @@
+package org.example;
+
+import com.sun.source.tree.TryTree;
+
+import javax.xml.crypto.Data;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server { // Classe server
+    ServerSocket server = null;
+    Socket socketClient = null;
+
+    int porta = 6789;  //Porta resa disponibile per la comunicazione con il client
+
+    PrintWriter out; //Permette la comunicazione di output con il client
+
+    BufferedReader in; // Permette la comunicazione di input con il client
+
+    String nomeUtente;
+
+    int root = 5; // Gestisce i permessi tra Utente registrato (vale 2) ed utente Anonimo (vale 0)
+
+
+    public Socket attendi(){ // Questo metodo serve per mettere il server in ascolto, aspetta fino a quando un client non si collega sulla porta
+        try {
+            System.out.println("Server On") ;
+            server = new ServerSocket(porta); // inizializzazione del servizio
+            System.out.println("Server in ascolto sulla porta: "+porta);
+            socketClient = server.accept();
+            System.out.println("Connessione stabilita");
+            in = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
+            out = new PrintWriter(socketClient.getOutputStream(), true);
+            accedi();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return socketClient;
+
+
+    }
+
+    public void accedi(){ //Questo metodo gescisce il tipo di accesso, Se anon chiama il metodo apposito, se invece e' login (cioe' utente regisrato) chiama il metodo di login
+        try {
+            String accesso=in.readLine();
+            String accessoLower = accesso.toLowerCase();
+            switch (accessoLower){
+                case "anon":
+                    gestisciAnon();
+                    break;
+                case "login":
+                    controlloLogin();
+                    break;
+            }
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+        comunica();
+    }
+
+    public void comunica(){ // Questo metodo e' il cuore del server, mette a disposizione del client la lista dei comandi ed in base ai comandi che gli vengono restituiti chiama i metodi per gestire le richieste
+        try {
+            switch (root){
+                case 2:
+                    out.println("Inserisci comando: Download, Upload, List, Rename,Delete,Logout"); // se root vale 2 (utente registrato) da la lista dei comandi per gli utenti registrati
+                    break;
+                case 0:
+                    out.println("Inserisci comando: List, Download,Logout"); // se root vale 0 da la lista apposita per gli utenti non registrati
+                    break;
+            }
+
+            String richiesta = in.readLine();
+            if (richiesta.equals("errore")){
+                comunica();
+            }
+            String LowerRichiesta=richiesta.toLowerCase();
+            System.out.println("test"+LowerRichiesta);
+
+
+            switch (LowerRichiesta){ // Questo switch case lancia i metodi per gestire le richieste del client
+                case "download" :
+                    scarica();
+                    break;
+                case "upload" :
+                    upload();
+                    break;
+                case "list" :
+                    stampaLista();
+                    break;
+                case "rename" :
+                    rinomina();
+                    break;
+                case "delete":
+                    cancella();
+                    break;
+                case "logout":
+                    disconnetti();
+                    break;
+            }
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public void gestisciAnon(){ // setta il root a 0 e rilancia il cuore del server (comunica)
+        root=0;
+        out.println("accesso come Anonimo effettuato con successo");
+        comunica();
+    }
+
+
+    public void controlloLogin(){ // Questo metodo legge dalla cartella del server: DataBase il nome del file .txt per controllare il nome ed il contenuto del file per controllare la password, se sono entrambi corretti setta il root a 2
+        try {
+
+            String posizione="./DataBase/";
+
+            String nome= in.readLine();
+            String pass = in.readLine();
+            String posizione2=posizione+nome+"//"+nome+".txt";
+            System.out.println(posizione2);
+
+            File fileUtente = new File(posizione2);
+
+            if (!fileUtente.exists()) { // Verifica se il file (utente) esiste
+                System.out.println("Utente non esistente.");
+                out.println("Utente non trovato");
+                accedi();
+            }
+
+            try (BufferedReader lettore = new BufferedReader(new FileReader(fileUtente))){
+                String password = lettore.readLine();
+                if (password.equals(pass)){
+                    root=2;
+                    out.println("ciao "+nome+" ti sei autentificato con successo");
+                    nomeUtente=nome;
+                    comunica();
+                } else {
+                    out.println("password errata");
+                    accedi();
+                }
+            }
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+
+        System.out.println("autenticazione successo");
+
+
+    }
+
+    public void scarica() { // Questo metodo gescisce le richieste di download, crea una nuova socket per permettere il download dei file, i file vengono scaricati dal server alla cartella del client
+        String nomeFile = null;
+        String posizione="";
+         if (root == 2){
+              posizione="DataBase//"+nomeUtente+"//cartella";
+         } else if (root==0){
+              posizione="DataBase//CartellaAnon";
+         }
+
+        try {
+            out.println("Quale file intendi scaricare?");
+            nomeFile = in.readLine();
+            posizione=posizione+"/"+nomeFile;
+            if (nomeFile == null || nomeFile.trim().isEmpty()) {
+                out.println("Nome del file non fornito.");
+                comunica();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        File file = new File(posizione);
+        if (!file.exists()) {
+            out.println("File non trovato");
+            comunica();
+        }
+
+        try (ServerSocket serverSocketDati = new ServerSocket(0)) { // 0 per selezionare una porta disponibile automaticamente
+            int portaDati = serverSocketDati.getLocalPort();
+            out.println(portaDati); // Comunica al client la porta per la connessione dati
+
+            try (Socket socketDati = serverSocketDati.accept();
+                 FileInputStream fis = new FileInputStream(file);
+                 BufferedOutputStream bos = new BufferedOutputStream(socketDati.getOutputStream())) {
+                byte[] buffer = new byte[4096];
+                int count;
+                while ((count = fis.read(buffer)) > 0) {
+                    bos.write(buffer, 0, count);
+                }
+            } catch (IOException e){
+                System.out.println(e.getMessage());
+            }
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+        comunica();
+    }
+
+    public void upload(){ // Questo metodo e' l'inverso del metodo download, permette l'upload dei file dal client al server
+        try {
+            String posizione="DataBase//"+nomeUtente+"//cartella//";
+            out.println("Inserisci il nome del file che intendi mandare al server");
+            String nome=in.readLine();
+            posizione=posizione+nome;
+
+            try (ServerSocket serverSocketDati = new ServerSocket(0)) {
+                int portaDati = serverSocketDati.getLocalPort();
+                out.println(portaDati); // Invia la porta al client
+
+                try (Socket socketDati = serverSocketDati.accept();
+                     BufferedInputStream bis = new BufferedInputStream(socketDati.getInputStream());
+                     FileOutputStream fos = new FileOutputStream(posizione)) {
+                    byte[] buffer = new byte[4096];
+                    int bytesRead;
+                    while ((bytesRead = bis.read(buffer)) != -1) {
+                        fos.write(buffer, 0, bytesRead);
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Errore durante la ricezione del file: " + e.getMessage());
+            }
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+        comunica();
+    }
+
+
+    public void stampaLista(){ // Questo metodo permette di stampare la lista dei comandi in base al tipo di accesso effettuato
+        String posizione = "";
+        switch (root){
+            case 0 :
+                System.out.println("sono anonimo");
+                posizione="./DataBase//CartellaAnon";
+                break;
+            case 2 :
+                System.out.println("ciao "+nomeUtente+"questa è la tua cartella");
+                posizione="DataBase//"+nomeUtente+"//cartella";
+                System.out.println(posizione);
+                break;
+        }
+
+        File directory = new File(posizione);
+        System.out.println(directory.getAbsolutePath());
+        File[] filesList = directory.listFiles();
+        if (filesList != null) {
+            for (File file : filesList) {
+                if (file.isFile()) {
+                    System.out.println("File: " + file.getName());
+                    out.println("File: " + file.getName());
+                }
+            }
+        } else {
+            System.out.println("Cartella non trovata");
+            out.println("END");
+        }
+        out.println("finito");
+        comunica();
+    }
+
+    public void rinomina(){ // Questo metodo permette di rinominare il nome dei file gia' presenti sul server
+        String posizione="DataBase//"+nomeUtente+"//cartella";
+
+
+        try {
+            out.println("Quale file vuoi rinominare??");
+            String nomeFile=in.readLine();
+            out.println("Inserisci il nuovo nome");
+            String nuovoNome=in.readLine();
+            File fileEsistente = new File(posizione+"/"+nomeFile);
+            System.out.println(posizione+nomeFile);
+
+            // Crea un oggetto File per il nuovo nome del file
+            File nuovoFile = new File(posizione+"/"+nuovoNome);
+
+            // Rinomina il file
+            boolean successo = fileEsistente.renameTo(nuovoFile);
+
+            if (successo) {
+                out.println(nomeFile+" rinominato con successo");
+            } else {
+                out.println("impossibile rinominare "+nomeFile);
+            }
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        comunica();
+    }
+
+    public void cancella(){ // Questo metodo permette di cancellare i file presenti sul server
+        System.out.println("sono qua");
+        out.println("Quale file vuoi cancellare??");
+        String posizione="DataBase//"+nomeUtente+"//cartella";
+        try {
+            String nome=in.readLine();
+            posizione=posizione+"/"+nome;
+            File fileDaEliminare=new File(posizione);
+            if (fileDaEliminare.delete()){
+                out.println("File eliminato correttamente");
+                comunica();
+            } else {
+                out.println("non e' stato possibile eliminare il file");
+            }
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public void disconnetti(){ // Questo metodo permette la disconnessione degli utenti, richiamando alla fine il metodo accedi per poter permettere un nuovo accesso
+        root=5;
+        nomeUtente="";
+        out.println("Disconnessione effettuata con successo");
+        accedi();
+    }
+
+
+    public static void main(String[] args) { // Il main lancia solo il metodo accedi che avvia il tutto
+        Server s = new Server();
+        s.attendi();
+    }
+}
